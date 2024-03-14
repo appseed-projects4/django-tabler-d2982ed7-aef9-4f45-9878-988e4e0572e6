5867ded4f49514760fdd147eee2c7c26c56140ba
AppSeed Generated Projects <133563814+appseed-projects4@users.noreply.github.com>
2024-03-14 18:13:27 +0200

    initial commit

---
+++ home/models.py
@@ -0,0 +1,44 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
+
+# Create your models here.
+
+class UserProfile(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    #__PROFILE_FIELDS__
+    name = models.CharField(max_length=255, null=True, blank=True)
+    phone = models.CharField(max_length=255, null=True, blank=True)
+    bio = models.CharField(max_length=255, null=True, blank=True)
+
+    #__PROFILE_FIELDS__END
+
+    def __str__(self):
+        return self.user.username
+    
+    class Meta:
+        verbose_name        = _("UserProfile")
+        verbose_name_plural = _("UserProfile")
+
+#__MODELS__
+class Antenna(models.Model):
+
+    #__Antenna_FIELDS__
+    model = models.CharField(max_length=255, null=True, blank=True)
+
+    #__Antenna_FIELDS__END
+
+    class Meta:
+        verbose_name        = _("Antenna")
+        verbose_name_plural = _("Antenna")
+
+
+
+#__MODELS__END
